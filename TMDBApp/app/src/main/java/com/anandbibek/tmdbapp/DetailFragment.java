722a884563df9c2817dbce82b0357f37b086a12e
anandbibek <ananda.bibek@gmail.com>
2016-01-01 01:34:17 +0530

    Staggered animation

--- TMDBApp/app/src/main/java/com/anandbibek/tmdbapp/DetailFragment.java
+++ TMDBApp/app/src/main/java/com/anandbibek/tmdbapp/DetailFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Interpolator;
 import android.widget.TextView;
 
 import com.anandbibek.tmdbapp.volley.CustomVolley;
@@ -16,7 +18,7 @@ public class DetailFragment extends Fragment {
 
     private static final String MOVIE_INFO_PARAM = "movie_info";
     NetworkImageView background, poster;
-    TextView bigTitle, smallTitle, TextView, dateText, ratingText, popularityText;
+    TextView bigTitle, smallTitle, dateText, ratingText, popularityText;
     ImageLoader imageLoader;
 
     public static DetailFragment newInstance(MovieInfo data) {
@@ -55,8 +57,39 @@ public class DetailFragment extends Fragment {
             ratingText.setText(String.format(getString(R.string.rating), info.rating, info.vote_count));
             popularityText.setText(String.format(getString(R.string.popularity), String.format("%.0f", info.popularity)));
         }
+        staggeredAnimate();
         return root;
     }
 
+    public void staggeredAnimate(){
+        View[] animatedViews = new View[] { bigTitle, dateText, ratingText, popularityText, smallTitle};
+
+        // see here for using the right interpolator is important:
+        // http://www.google.com/design/spec/animation/authentic-motion.html#authentic-motion-mass-weight
+        // and here for how to use them:
+        // http://developer.android.com/guide/topics/graphics/prop-animation.html#interpolators
+        Interpolator interpolator = new DecelerateInterpolator();
+
+        for (int i = 0; i < animatedViews.length; ++i) {
+            View v = animatedViews[i];
+
+            // let's enable hardware acceleration for better performance
+            // http://blog.danlew.net/2015/10/20/using-hardware-layers-to-improve-animation-performance/
+            v.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+
+            // initial state: hide the view and move it down slightly
+            v.setAlpha(0f);
+            v.setTranslationY(75);
+
+            v.animate()
+                    .setInterpolator(interpolator)
+                    .alpha(1.0f)
+                    .translationY(0)
+                            // this little calculation here produces the staggered effect we
+                            // saw, so each animation starts a bit after the previous one
+                    .setStartDelay(300 + 75 * i)
+                    .start();
+        }
+    }
 
 }
